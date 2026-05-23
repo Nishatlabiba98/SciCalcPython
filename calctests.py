c2813545d6f287b57028a16e92cea8b1937912f4
nishat <nishatlabiba98@gmail.com>
2026-05-22 21:39:04 -0400

    def setUp self.calc

--- calctests.py
+++ calctests.py
@@ -3,21 +3,17 @@ import math
 from calculator import Calculator
 
 class TestCoreFeatures (unittest.TestCase) :
-    def test_add(self):
-        c = Calculator()
-        self.assertEqual(c.add(3, 3), 6)
+    def setUp(self):
+        self.calc = Calculator()
 
     def test_add2(self):
-        c = Calculator()
-        self.assertEqual(c.add(12, -10), 2)
+        self.assertEqual(self.calc.add(12, -10), 2)
 
     def test_add3(self):
-        c = Calculator()
-        self.assertEqual(c.add(5, 8), 13)
+        self.assertEqual(self.calc.add(5, 8), 13)
 
     def test_sub(self):
-        c = Calculator()
-        self.assertEqual(c.sub(9, 3), 6)
+        self.assertEqual(self.calc.sub(9, 3), 6)
 
 
 if __name__ == '__main__':
