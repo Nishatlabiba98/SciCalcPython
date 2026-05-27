88222447dd6b332fb79bfce6a59606c5cc7cbee0
nishat <nishatlabiba98@gmail.com>
2026-05-27 07:47:26 -0400

    Ran 5 tests in 0.000s

--- calctests.py
+++ calctests.py
@@ -8,6 +8,13 @@ class TestCoreFeatures (unittest.TestCase) :
 
     def test_initial_state(self):
         self.assertEqual(self.calc.state, 0)
-    
-
-
+    def test_add_twonumbers(self):
+        self.assertEqual(self.calc.add(3, 3), 6)
+    def test_subtract_twonumbers(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+    def test_multiply_twonumbers(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+    def test_divide_twonumbers(self):
+        self.assertEqual(self.calc.divide(40, 5), 8)
+if __name__ == '__main__':
+    unittest.main()
