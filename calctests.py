b100436e827af3f0a24cafffe6dfd78ead81c1d9
nishat <nishatlabiba98@gmail.com>
2026-05-27 07:37:00 -0400

    removed some stuff to rewrite on my own

--- calctests.py
+++ calctests.py
@@ -9,16 +9,5 @@ class TestCoreFeatures (unittest.TestCase) :
     def test_initial_state(self):
         self.assertEqual(self.calc.state, 0)
     
-    def test_get_display(self):
-        self.calc.state = 5
-        self.assertEqual(self.calc.get_display(), 5)
-
-    def test_add3(self):
-        self.assertEqual(self.calc.add(5, 8), 13)
-
-    def test_sub(self):
-        self.assertEqual(self.calc.sub(9, 3), 6)
 
 
-if __name__ == '__main__':
-    unittest.main()
