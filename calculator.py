f581f469954983b86329ba2af2fdda54b688f08d
nishat <nishatlabiba98@gmail.com>
2026-05-31 10:51:33 -0400

    mcos, tan, log, ln

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import math # the math module provides access to mathematical functions
 
-class calculator:
+class Calculator:
     def __init__(self):
         self.state = 0
         self.error = False
@@ -24,6 +24,7 @@ class calculator:
             self.display = a + self.state
         else:
             self.display = a + b
+            self.state = self.display  
         return self.display
     
     def subtract(self, a, b= None):
@@ -33,44 +34,112 @@ class calculator:
             self.display = self.state -a
         else:
             self.display = a - b
+            self.state = self.display  
         return self.display
         
     def multiply(self, a, b=None):
         if self.error:
             return "Error"
         if b is None:
-                self.display = a * self.state
-        else: self.display = a * b
+            self.display = a * self.state
+        else: 
+            self.display = a * b
+            self.state = self.display
         return self.display
         
     def divide(self, a, b=None):
-            if self.error:
-                return"Error"
-                if b is None:
-                    if a == 0:
-                        self.error = True
-                        return "Error"
-                    self.display = self.state / a
-                else:
+        if self.error:
+            return "Error"
+        if b is None:
+            if a == 0:
+             self.error = True
+             return "Error"
+            self.display = self.state / a
+        else:
                     if b == 0:
-                        self.error = True
-                        return "Error"
-                    self.display = a / b
-            return self.display
+                     self.error = True
+                    return "Error"
+        self.display = a / b
+        self.state = self.display
+        return self.display
+
     def power(self, a, b= None):
         if self.error:
             return "Error"
         if b is None:
-            self.display = a ** self.state 
-        else:    
+            self.display = a ** self.state
+        else:
             self.display = a ** b
-            return self.display
-        
+        self.state = self.display
+        return self.display
+
     def sqrt(self, a):
-        if self.error: 
+        if self.error:
             return "Error"
         if a<0:
             self.error = True
             return "Error"
         self.display = math.sqrt(a)
+        self.state = self.display
         return self.display
+    
+    def sin(self, a, degrees=False):
+        if self.error:
+            return "Error"
+        if degrees: 
+            a = math.radians(a)
+        self.display = math.sin(a)
+        self.state = self.display
+        return self.display
+    
+    def cosine(self, a, degrees=False):
+        if self.error:
+            return "Error"
+        if degrees:
+            a = math.radians(a)
+            self.display = math.cos(a)
+        return self.display
+    
+    def tangent(self, a, degrees = False):
+        if self.error:
+            return "Error"
+        if degrees:
+            a = math.radians(a)
+        self.display = math.tan(a)
+        self.state = self.display
+        return self.display
+
+    def log(self, a):
+        if self.error:
+            return "Error"
+        if a <= 0:
+            self.error = True
+            return "Error"
+        self.display = math.log10(a)
+        self.state = self.display
+
+        return self.display 
+    
+    def ln(self, a):
+        if self.error:
+            return "Error"
+        if a <= 0:
+            self.error = True
+            return "Error"
+        self.display = math.log(a)
+        self.state = self.display
+        return self.display
+    
+if __name__ == '__main__':
+        calc = Calculator()
+        print(calc.add(2,3))
+        print(calc.subtract(10,4))
+        print(calc.multiply(2,3))
+        print(calc.divide(30,5))
+        print(calc.sqrt(16))
+        print(calc.power(2,3))
+        print(calc.sin(90, degrees= True))
+        print(calc.cosine(0, degrees= True))
+        print(calc.tangent(45, degrees= True))
+        print(calc.log(100))
+        print(calc.ln(math.e))
